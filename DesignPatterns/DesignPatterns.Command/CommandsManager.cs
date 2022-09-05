5ecf204349f36104b0a5e82a88759f4c4a258826
George Maina <george.maina@finova.tech>
2022-09-05 09:46:05 +0200

    added command tests

---
+++ DesignPatterns/DesignPatterns.Command/CommandsManager.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.Command
+{
+    public class CommandsManager : ICommand
+    {
+        private readonly Stack<ICommand> _commands = new();
+        public void AddCommand(ICommand command)
+        {
+            _commands.Push(command);
+        }
+
+        public void Execute()
+        {
+            foreach (var command in _commands)
+                command.Execute();
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns.Tests/CommandTests.cs
@@ -0,0 +1,56 @@
+﻿using DesignPatterns.Command;
+
+namespace DesignPatterns.Tests
+{
+    public class CommandTests
+    {
+        private readonly BankAccount _bankAccount;
+        private readonly DepositCommand _depositCommand;
+        private readonly WithdrawCommand _withdrawCommand;
+        private readonly CommandsManager _commandsManager;
+
+        public CommandTests()
+        {
+            _bankAccount = new BankAccount("Google");
+            _depositCommand = new DepositCommand(_bankAccount, 100);
+            _withdrawCommand = new WithdrawCommand(_bankAccount, 50);
+            _commandsManager = new CommandsManager();
+        }
+
+        [Fact]
+        public void BankAccount_StartsWith_ZeroBalance()
+        {
+            Assert.Equal(0, _bankAccount.Balance);
+        }
+
+        [Fact]
+        public void DepositCommand_IncreasesBalance_ByGivenAmount()
+        {
+            _depositCommand.Execute();
+            Assert.Equal(_bankAccount.Balance, _depositCommand.Amount);
+        }
+
+        [Fact]
+        public void WithdrawCommand_DecreasesBalance_ByGivenAmount()
+        {
+            _depositCommand.Execute();
+            var newBalance = _bankAccount.Balance;
+
+            _withdrawCommand.Execute();
+            Assert.Equal(_bankAccount.Balance, newBalance - _withdrawCommand.Amount);
+        }
+
+        [Fact]
+        public void ManagerCan_ExecuteAllCommands_AtOnce()
+        {
+            var initialBalance = _bankAccount.Balance;
+            var expectedFinalBalance = initialBalance + _depositCommand.Amount - _withdrawCommand.Amount;
+
+            _commandsManager.AddCommand(_withdrawCommand);
+            _commandsManager.AddCommand(_depositCommand);
+            _commandsManager.Execute();
+
+            Assert.Equal(_bankAccount.Balance, expectedFinalBalance);
+        }
+    }
+}
