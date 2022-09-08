8f8c774b54116edd71acb23a33fa964656a05009
George Maina <george.maina@finova.tech>
2022-09-08 12:33:12 +0200

    added chain of responsibility

---
+++ DesignPatterns/DesignPatterns.ChainOfResponsibility/Request.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns.ChainOfResponsibility
+{
+    public class Request
+    {
+        private readonly List<string> _headers = new();
+
+        public Request(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public bool ContainsHeader(string header) => _headers.Contains(header);
+        public void AddHeader(string header) => _headers.Add(header);
+    }
+}
---
+++ DesignPatterns/DesignPatterns.ChainOfResponsibility/RequestLogger.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.ChainOfResponsibility
+{
+    public class RequestLogger : RequestHandler
+    {
+        public RequestLogger(RequestHandler? handler) : base("Request Logger", handler)
+        {
+        }
+
+        public bool LogginSucceeded { get; set; } = true;
+        protected override bool HandleRequest(Request request)
+        {
+            return LogginSucceeded;
+        }
+    }
+}
