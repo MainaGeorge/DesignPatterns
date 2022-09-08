7fbdae6780b5e750e2d69fe0f5c0fe84ca9e950c
George Maina <george.maina@finova.tech>
2022-09-08 10:19:19 +0200

    added visitor pattern

---
+++ DesignPatterns/DesignPatterns.Tests/VisitorPatternTests.cs
@@ -0,0 +1,35 @@
+﻿using DesignPatterns.Visitor.Host;
+using DesignPatterns.Visitor.Visitor;
+
+namespace DesignPatterns.Tests
+{
+    public class VisitorPatternTests
+    {
+        [Fact]
+        public void VisitorCalculatesCashBackCorrectly()
+        {
+            var goldCard = new GoldCard();
+            var platinumCard = new PlatinumCard();
+
+            var gasOffer = new GasOffer();
+            var hotelOffer = new HotelBookingOffer();
+
+            var expectedGasOfferGoldCard = gasOffer.GetCashBack(goldCard);
+            var expectedGasOfferPlatinumCard = gasOffer.GetCashBack(platinumCard);
+
+            var expectedHotleOfferGoldCard = hotelOffer.GetCashBack(goldCard);
+            var expectedHotleOfferPlatinumCard = hotelOffer.GetCashBack(platinumCard);
+
+            var actualGasOfferGoldCard = goldCard.Accept(gasOffer);
+            var actualGasOfferPlatinumCard = platinumCard.Accept(gasOffer);
+
+            var actualHotelOfferGoldCard = goldCard.Accept(hotelOffer);
+            var actualHotelOfferPlatinumCard = platinumCard.Accept(hotelOffer);
+
+            Assert.Equal(expectedGasOfferPlatinumCard, actualGasOfferPlatinumCard);
+            Assert.Equal(expectedGasOfferGoldCard, actualGasOfferGoldCard);
+            Assert.Equal(expectedHotleOfferGoldCard, actualHotelOfferGoldCard);
+            Assert.Equal(expectedHotleOfferPlatinumCard, actualHotelOfferPlatinumCard);
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns.Visitor/Host/BankCard.cs
@@ -0,0 +1,16 @@
+﻿using DesignPatterns.Visitor.Visitor;
+
+namespace DesignPatterns.Visitor.Host
+{
+    public abstract class BankCard
+    {
+        public string Name { get; protected set; }
+
+        public abstract string Accept(VisitorOffer offer);
+
+        protected BankCard(string name)
+        {
+            Name = name;
+        }
+    }
+}
